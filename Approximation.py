e96890bcd4817b554dd2c54759561f78c3e90610
hatuw <jiaxi_wu@ieee.org>
2018-04-19 15:50:41 +0800

    update code

--- Approximation.py
+++ Approximation.py
@@ -36,8 +36,10 @@ def find_d(*args):
     less than(<) or greater than(>)
     (f1-f2)/(g1-g2) and -(f1,f2)/(g1+g2)
     """
-    d = 1
-    assert len(args) == 2, "It need tow args(f, g) to find d"
+    lowwer_bound = args[2]+1 if args[2] % 2 == 0 else args[2]
+    upper_bound = args[3]
+    d = np.arange(lowwer_bound, upper_bound, 2)[:, np.newaxis]
+    assert len(args) == 4, "It need tow args(f, g) to find d"
     d = phi(args[0] + d*args[1])
     return d
 
@@ -50,15 +52,18 @@ def minimize(f=None, d=None, g=None):
 a_k = 1 # $a_{k-1}$
 alpha = a_k * (2**(k-1))
 f = np.array([0, 2])
-g = np.array([2**k, 1])
+g = np.array([2**(k-1), 1])
+print(a_k, alpha, f, g)
 
 i = 0
 while i < 10:
     # input a new bits a_k
+    """
     for index, val in enumerate(sequence):
         if val == '1':
             k = index + 1
             break
+    """
 
     alpha += a_k * (2**k)
     if alpha*g[1] - g[0] // 2**(k+1) == 0:
@@ -66,13 +71,19 @@ while i < 10:
 
     elif phi(g) < phi(f):
         # let d to be odd and minimize phi(f+dg)
-        d = find_d(f, g)
+        temp_bound1 = (f[0] - f[1]) / (g[0] - g[1])
+        temp_bound2 = (f[0] + f[1]) / (g[0] + g[1])
+        bound = np.sort([temp_bound1, temp_bound2])
+        d = find_d(f, g, bound[0], bound[1])
         g = f + d*g
         f = 2 * g
 
     else:
         # let d to be odd and minimize phi(f+dg)
-        d = find_d(g, f)
+        temp_bound1 = (g[0] - g[1]) / (f[0] - f[1])
+        temp_bound2 = (g[0] + g[1]) / (f[0] + f[1])
+        bound = np.sort([temp_bound1, temp_bound2])
+        d = find_d(g, f, bound[0], bound[1])
         g = f + d*f
         f *= 2
     k += 1
